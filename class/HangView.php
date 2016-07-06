cbe99fa1a630a687ea357fa9c99d060e28773a36
root <wjasciutto@gmail.com>
2016-07-06 13:57:21 -0400

    Sets up necessary variables and functions. Last time test view functions before actual implementation begins.

--- class/HangView.php
+++ class/HangView.php
@@ -10,20 +10,58 @@ class HangView
       $this->hangman = $hangman;
     }
 
+    /*
+    Sets up the PHPWS_Template by filling in $tpl fields.
+    */
     public function display()
     {
       $pic = 'mod/hangman/img/hang6.gif';
       $picture[] = array('PICTURE' => "<img src=" . $pic . " />");
 
       //Module link takes text to display, module name, and an array of URL attributes to access with $_GET
+      //$available needs the name of the desired template holder and content to place
       $avail = PHPWS_Text::moduleLink('a','hangman',array('action'=>'guess','letter'=>'a'));
       $available[] = array('LETTERS' => $avail);
 
+      //$tpl refers to lowercase comment tag names in hangview.tpl
       $tpl['pic'] = $picture;
       $tpl['letters'] = $available;
       //$tpl['cLetters'] =
 
       return PHPWS_Template::process($tpl, self::MODULE, self::FILE);
+    }
+
+    /*
+    Determines the hangman image based on $hangman's $count.
+    */
+    public function getImage()
+    {
+
+    }
+
+    /*
+    Determines the letter panel display based on which
+    letters have been correctly guessed.
+    */
+    public function getLetterPanel()
+    {
+
+    }
+
+    /*
+    Determines how to display the guess form.
+    */
+    public function getGuessForm()
+    {
+
+    }
+
+    /*
+    Determines how to display the letter bank.
+    */
+    public function getLetterBank()
+    {
+
     }
 }
 ?>
--- class/Hangman.php
+++ class/Hangman.php
@@ -1,7 +1,104 @@
 <?php
   class Hangman
   {
+    #Word to guess as a character array, associated
+    #with a boolean to determine if a particular char
+    #should be revealed.
+    private $word;
+    #Bank of letters to guess as character array.
+    private $bank;
+    #Correctly guessed letters, includes each instance
+    #of letters that appear multiple times.
+    private $correct;
+    #The current hangman count. 6 = game over!
+    private $count;
+    #The current action.
+    private $action;
+
     public function __construct()
+    {
+
+    }
+
+    /*
+    Fetches a random word from hangwords.txt. Stores the
+    word as a character array in $_Session["word"]
+    */
+    public function fetchWord()
+    {
+
+    }
+
+    /*
+    Sets global variables to match super global variables.
+    */
+    public function updateSession()
+    {
+
+    }
+
+    /*
+    Uses $_GET to determine the current action
+    */
+    public function updateAction()
+    {
+
+    }
+
+    /*
+    Re-initializes the game. All super global variables
+    cleared.
+    */
+    public function resetGame()
+    {
+
+    }
+
+    /*
+    Increases the hang count by one.
+    */
+    public function incrementHangCount()
+    {
+
+    }
+
+    /*
+    Removes a letter from the letterbank.
+    */
+    public function bankRemoveLetter($letter)
+    {
+
+    }
+
+    /*
+    Checks to see if a letter is in the word to guess.
+    */
+    public function checkLetter($letter)
+    {
+
+    }
+
+    /*
+    Updates $word to indicate that the input letter
+    has been revealed.
+    */
+    public function revealLetter($letter)
+    {
+
+    }
+
+    /*
+    Sets all of $word to true, revealing the whole word.
+    */
+    public function revealWord()
+    {
+
+    }
+
+    /*
+    Determines if the game is over (just checks $count)
+    */
+    public function isGameOver()
     {
 
     }
