55526f1c63cdc0198a1f93a88fca8b60e1215d1a
Mollie Gentry <mgentry386@gmail.com>
2016-09-12 17:15:57 -0400

    Add HTML and session start

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
-<?php
-  \Layout::add("Hello World");
-  ob_start();
-  include "hangwords.txt";
-  $words = ob_get_contents();
-  ob_end_clean();
-  $words = trim($words);
-  $words = strtolower($words);
+<html>
+<head>
+  <body>
+    <img src="hang0.gif" />
+    <form action="welcome.php" method="post">
+Pick a letter: <input type="text" name="letter"><br>
+<input type="submit">
+    </form>
+    <?php
+      session_start(['cookie_lifetime' => 86400,]);
+      ob_start();
+      include "hangwords.txt";
+      $words = ob_get_contents();
+      ob_end_clean();
+      $words = trim($words);
+      $words = strtolower($words);
+      $choice = $_GET["letter"];
+      //echo '<img src="hang0.gif" />';//\
+
+      //fxn to run game
+      function run_game($words, $letter){
+        
+      }
+    ?>
+  </body>
+</head>
+</html>
