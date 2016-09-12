b5c2779bf39662e1b038207ea934a12ecfb4aef5
Mollie Gentry <mgentry386@gmail.com>
2016-09-12 12:42:10 -0400

    Add, Trim and LowerCase Hangswords.txt

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-\Layout::add("Hello World");
+  \Layout::add("Hello World");
+  ob_start();
+  include "hangwords.txt";
+  $words = ob_get_contents();
+  ob_end_clean();
+  $words = trim($words);
+  $words = strtolower($words);
