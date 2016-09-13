f9c7d1bf9006670608321191b212cf35357edcd6
Mollie Gentry <mgentry386@gmail.com>
2016-09-13 14:20:26 -0400

    Separate html and php, correct file read, choose word

--- index.php
+++ index.php
@@ -1,27 +1,11 @@
-<html>
-<head>
-  <body>
-    <img src="hang0.gif" />
-    <form action="welcome.php" method="post">
-Pick a letter: <input type="text" name="letter"><br>
-<input type="submit">
-    </form>
-    <?php
-      session_start(['cookie_lifetime' => 86400,]);
-      ob_start();
-      include "hangwords.txt";
-      $words = ob_get_contents();
-      ob_end_clean();
-      $words = trim($words);
-      $words = strtolower($words);
-      $choice = $_GET["letter"];
-      //echo '<img src="hang0.gif" />';//\
+<?php
+  $word_list = file(PHPWS_SOURCE_DIR . 'mod/hangman/hangwords.txt');
+  $word = $word_list[array_rand($word_list, 1)];
+  $word = strtolower($word);
+  $word = preg_replace('/[^a-z]/', '', $word);
 
-      //fxn to run game
-      function run_game($words, $letter){
-        
-      }
-    ?>
-  </body>
-</head>
-</html>
+  //fxn to run game
+  function run_game($words, $letter){
+    $choice = $_GET["letter"];
+  }
+?>
---
+++ templates/game.tpl
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <body>
+
+    <img src="hang0.gif" />
+
+    <form action="index.php" method="get">
+Pick a letter: <input type="text" name="letter"><br>
+<input type="submit">
+    </form>
+
+  </body>
+</head>
+</html>
