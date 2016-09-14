432a56ffc892fdaf8d311161d9523b333c520133
Mollie Gentry <mgentry386@gmail.com>
2016-09-14 16:04:28 -0400

    Add a whole bunch of run_game fxn conditionals and attempts variable

--- index.php
+++ index.php
@@ -6,14 +6,45 @@
 
   $template['FORM_CONTENT'] = 'Pick a letter: ';
   $template['GREETING'] = 'Welcome to Hangman';
+  $template['IMG_SRC'] = file(PHPWS_SOURCE_DIR . 'mod/hangman/img/hang0.gif');
 
   echo PHPWS_Template::process($template, 'hangman','game.tpl');
 
+
+  $attempts = 0;
   //fxn to run game
   function run_game($word){
-    $choice = $_GET["letter"];
+    $letter_choice = $_POST['letter'];
 
-    $choice = $choice[0];
+    if(strlen($letter_choice) > 1){
+      echo "Please, only choose 1 letter.";
+      echo PHPWS_Template::process($template, 'hangman','game.tpl');
+    }
+    else{
+      for($i = 0; $i <= strlen($word); $i++){
+        echo "_ ";
+      }
+
+      while($attempts < 7){
+        $pos = strpos($word, $letter_choice);
+        if($pos === false){
+          echo "Your letter was not part of the word.";
+          $attempts = $attempts + 1;
+
+          $template['FORM CONTENT'] = 'Pick another letter: ';
+          $template['GREETING'] = 'Continue your game. You have ' . (6 - $attempts) . ' attempts left.';
+          $template['IMG_SRC'] = file(PHPWS_SOURCE_DIR . 'mod/hangman/img/hang1.gif');
+
+          echo PHPWS_Template::process($template, 'hangman','game.tpl');
+
+        }
+        else{
+          echo "Your letter was found in the word!";
+          
+        }
+      }
+
+    }
   }
 
 ?>
--- templates/game.tpl
+++ templates/game.tpl
@@ -3,11 +3,11 @@
   <body>
 
     <h1 class="title">{GREETING}</h1>
-    <img src="hang0.gif" />
+    <img src={IMG_SRC} />
 
-    <form action="index.php" method="get">
+    <form action="index.php" method="post">
 {FORM_CONTENT}<input type="text" name="letter"><br>
-<input type="submit">
+<input type="submit" name="letterSubmit" value="Submit">
     </form>
 
   </body>
