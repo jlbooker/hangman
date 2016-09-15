793190fe5365b0c79f08c25e62f5c2803ba691ef
Mollie Gentry <mgentry386@gmail.com>
2016-09-15 13:05:47 -0400

    Add hang inamges to array, style to html, more conditional adjustments

--- index.php
+++ index.php
@@ -3,12 +3,16 @@
   $word = $word_list[array_rand($word_list, 1)];
   $word = strtolower($word);
   $word = preg_replace('/[^a-z]/', '', $word);
+  $images = [file(PHPWS_SOURCE_DIR . 'mod/hangman/img/hang0.gif'), file(PHPWS_SOURCE_DIR . 'mod/hangman/img/hang1.gif'),
+    file(PHPWS_SOURCE_DIR . 'mod/hangman/img/hang2.gif'), file(PHPWS_SOURCE_DIR . 'mod/hangman/img/hang3.gif'),
+    file(PHPWS_SOURCE_DIR . 'mod/hangman/img/hang4.gif'), file(PHPWS_SOURCE_DIR . 'mod/hangman/img/hang5.gif'),
+    file(PHPWS_SOURCE_DIR . 'mod/hangman/img/hang6.gif')];
   $attempts = 0;
   $place_holder = "_ ";
 
   $template['FORM_CONTENT'] = 'Pick a letter: ';
   $template['GREETING'] = 'Welcome to Hangman';
-  $template['IMG_SRC'] = file(PHPWS_SOURCE_DIR . 'mod/hangman/img/hang0.gif');
+  $template['IMG_SRC'] = $images[$attempts];
 
   echo PHPWS_Template::process($template, 'hangman','game.tpl');
 
@@ -24,7 +28,8 @@
   //fxn to run game
   function run_game($word){
     $letter_choice = $_POST['letter'];
-    global $word, $place_holder, $attempts;
+    global $word, $attempts;
+    $blank_spaces = blanks($words);
 
     if(strlen($letter_choice) > 1){
       echo "Please, only choose 1 letter.";
@@ -44,8 +49,8 @@
 
           $template['FORM CONTENT'] = 'Pick another letter: ';
           $template['GREETING'] = 'Continue your game. You have ' . (6 - $attempts) . ' attempts left.';
-          $template['IMG_SRC'] = file(PHPWS_SOURCE_DIR . 'mod/hangman/img/hang1.gif'); //put imgs in array
-          $template['BLANKS_WORD'] = blanks($word);
+          $template['IMG_SRC'] = $images[$attempts];
+          $template['BLANKS_WORD'] = $blank_spaces;
 
           echo PHPWS_Template::process($template, 'hangman','game.tpl');
         }
@@ -54,13 +59,21 @@
 
           for($i = 0; $i <= strlen($word); $i++){
             if($word[i] == $letter_choice){
-              $plac_holder[$i] = $letter_choice;
+              $blank_spaces[$i] = $letter_choice;
             }
           }
+
+          $template['FORM CONTENT'] = 'Pick another letter: ';
+          $template['GREETING'] = 'Continue your game. You have ' . (6 - $attempts) . ' attempts left.';
+          $template['IMG_SRC'] = $images[$attempts];
+          $template['BLANKS_WORD'] = $blank_spaces;
+
+          echo PHPWS_Template::process($template, 'hangman','game.tpl');
         }
       }
-
     }
   }
+
+  run_game($word);
 
 ?>
--- templates/game.tpl
+++ templates/game.tpl
@@ -3,13 +3,13 @@
   <title>the one where you hang people</title>
   <body>
 
-    <h1 class="title">{GREETING}</h1>
+    <h1 style="text-align: center" class="title">{GREETING}</h1>
     <img src={IMG_SRC} />
     <p>{BLANKS_WORD}</p>
 
-    <form action="index.php" method="post">
+    <form style="text-align: center" action="index.php" method="post">
 {FORM_CONTENT}<input type="text" name="letter"><br>
-<input type="submit" name="letterSubmit" value="Submit">
+<input style="margin-top: 10px" type="submit" name="letterSubmit" value="Submit">
     </form>
 
   </body>
