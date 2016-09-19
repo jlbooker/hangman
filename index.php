f53ba27752922adec7fe24429dd9200085a54ab4
Mollie Gentry <mgentry386@gmail.com>
2016-09-19 17:21:09 -0400

    add session variable for random word, add alphabet loop

--- index.php
+++ index.php
@@ -3,12 +3,23 @@
   $word = $word_list[array_rand($word_list, 1)];
   $word = strtolower($word);
   $word = preg_replace('/[^a-z]/', '', $word);
+  $_SESSION['word_ses'] = $word;
   $attempts = 0;
   $place_holder = "_ ";
+
+  //fxn to display alphabet links
+  function alphabet_list(){
+    for ($i = 65; $i <= 90; $i++) {
+    printf('<a href="/phpwebsite/index.php?module=hangman&letter=%1$s" class="myclass">%1$s</a> ', chr($i));
+    global $_SESSION['letter'] = chr($i);
+    }
+  }
 
   $template['FORM_CONTENT'] = 'Pick a letter: ';
   $template['GREETING'] = 'Welcome to Hangman';
   $template['IMG_SRC'] = "http://localhost/phpwebsite/mod/hangman/img/hang$attempts.gif";
+  $template['BLANKS_WORD'] = blanks('word_ses');
+  $template['ALPHABET'] = alphabet_list();
 
   //$template['IMG_SRC'] = 'http://localhost/phpwebsite/mod/hangman/img/hang' . $attempts . 'gif';
 
@@ -25,31 +36,18 @@
 
   //fxn to run game
   function run_game($word){
-    if(isset($_POST['submit'])){
-
-    $letter_choice = $_POST['letter'];
     global $word, $attempts;
     $blank_spaces = blanks($words);
 
-    if(strlen($letter_choice) > 1){
-      $template['REPONSE'] = 'Please, only choose 1 letter.';
-
-      echo PHPWS_Template::process($template, 'hangman','game.tpl');
-    }
-    else{
-      for($i = 0; $i <= strlen($word); $i++){
-        echo "_ ";
-      }
-
       while($attempts < 7){
-        $pos = strpos($word, $letter_choice);
+        $pos = strpos($word, 'letter');
 
         if($pos === false){
           $attempts = $attempts + 1;
 
           $template['FORM CONTENT'] = 'Pick another letter: ';
           $template['RESPONSE'] = 'Continue your game. You have ' . (6 - $attempts) . ' attempts left.';
-          $template['IMG_SRC'] = $images[$attempts];
+          $template['IMG_SRC'] = "http://localhost/phpwebsite/mod/hangman/img/hang$attempts.gif";
           $template['BLANKS_WORD'] = $blank_spaces;
           $template['GREETING'] = 'Your letter was not part of the word.';
 
@@ -65,7 +63,7 @@
 
           $template['FORM CONTENT'] = 'Pick another letter: ';
           $template['GREETING'] = 'Continue your game. You have ' . (6 - $attempts) . ' attempts left.';
-          $template['IMG_SRC'] = $images[$attempts];
+          $template['IMG_SRC'] = "http://localhost/phpwebsite/mod/hangman/img/hang$attempts.gif";
           $template['BLANKS_WORD'] = $blank_spaces;
           $template['RESPONSE'] = 'Your letter was found in the word!';
 
@@ -73,9 +71,7 @@
         }
       }
     }
-  }
-}
 
-run_game($word);
+run_game('word_ses');
 
 ?>
--- templates/game.tpl
+++ templates/game.tpl
@@ -6,17 +6,16 @@
   .title {text-align: center; font-size: 60px;}
   h3 {text-align: center;}
   p {text-align: center;}
+
   </style>
 
     <h1 class="title">{GREETING}</h1>
     <h3 >{RESPONSE}</h3>
     <center><img src={IMG_SRC} /></center>
-    <p >{BLANKS_WORD}</p>
+    <p>{BLANKS_WORD}</p>
 
-    <form style="text-align: center" action="index.php" method="post">
-{FORM_CONTENT}<input type="text" name="letter"><br>
-<input style="margin-top: 10px" type="submit" name="letterSubmit" value="Submit">
-    </form>
+    <h3>Choose a letter from the list below...</h3>
+    <center><ul>{ALPHABET}</ul></center>
 
   </body>
 </head>
