0e898e8307663c1540edbe2ace16272111e162ef
Mollie Gentry <mgentry386@gmail.com>
2016-09-21 17:14:41 -0400

    Change hangman.php to Hangman.php, get template working from Hangman class

---
+++ Hangman.php
@@ -0,0 +1,92 @@
+<?php
+  class Hangman{
+    private $_SESSION;
+    // private $_SESSION['letterLinks'];
+    // private $_SESSION['attempts'];
+
+    //constructor
+    function __construct($word, $attempts){
+      $_SESSION['word'] = $word;
+      $_SESSION['attempts'] = $attempts;
+      define("PLACEHOLDER", "_ ");
+
+      $template['FORM_CONTENT'] = 'Pick a letter: ';
+      $template['GREETING'] = 'Welcome to Hangman';
+      $template['IMG_SRC'] =
+        "http://localhost/phpwebsite/mod/hangman/img/hang{$_SESSION['attempt']}.gif";
+      $template['BLANKS_WORD'] = $this->blanks($_SESSION['word']);
+      $template['ALPHABET'] = $this->alphabetList();
+
+      echo PHPWS_Template::process($template, 'hangman','game.tpl');
+    }
+
+    function run_game(){
+
+        while($attempts > 0 && $attempts < 7){
+          $pos = strpos($word, $_SESSION['letterlinks']);
+
+          if($pos === false){
+            $attempts = $attempts + 1;
+
+            $template['FORM CONTENT'] = 'Pick another letter: ';
+            $template['RESPONSE'] = 'Continue your game.
+              You have ' . (6 - $attempts) . ' attempts left.';
+            $template['IMG_SRC'] =
+              "http://localhost/phpwebsite/mod/hangman/img/hang$attempts.gif";
+            $template['BLANKS_WORD'] = $blank_spaces;
+            $template['GREETING'] = 'Your letter was not part of the word.';
+
+            echo PHPWS_Template::process($template, 'hangman','game.tpl');
+          }else{
+
+            for($i = 0; $i <= strlen($word); $i++){
+              if($word[$i] == $_SESSION['letterlinks']){
+                $blank_spaces[$i] = $_SESSION['letterlinks'];
+              }
+            }
+
+            $template['FORM CONTENT'] = 'Pick another letter: ';
+            $template['GREETING'] = 'Continue your game.
+              You have ' . (6 - $attempts) . ' attempts left.';
+            $template['IMG_SRC'] =
+              "http://localhost/phpwebsite/mod/hangman/img/hang$attempts.gif";
+            $template['BLANKS_WORD'] = $blank_spaces;
+            $template['RESPONSE'] = 'Your letter was found in the word!';
+
+            echo PHPWS_Template::process($template, 'hangman','game.tpl');
+          }
+        }
+
+        if($pos = strpos($blank_spaces, '_ ')){
+          $template['FORM CONTENT'] = 'You lost the game!';
+          $template['GREETING'] = 'Better luck next time.';
+          $template['IMG_SRC'] =
+            "http://localhost/phpwebsite/mod/hangman/img/hang6.gif";
+          $template['BLANKS_WORD'] = word_ses;
+          $template['RESPONSE'] = 'Click here to start a new game:
+            <a href="/phpwebsite/index.php?module=hangman">New Game</a>';
+
+          echo PHPWS_Template::process($template, 'hangman','game.tpl');
+        }
+      }
+
+
+    //fxn to display alphabet links
+    function alphabetList(){
+      for ($i = 65; $i <= 90; $i++) {
+        $letterLinks[] = '<a href="/phpwebsite/index.php?module=hangman&letter='.chr($i).'" class="myclass">'.chr($i).'</a>';
+      }
+      $_SESSION['letterLinks'] = $letterLinks;
+      return implode(" ",$letterLinks);
+    }
+
+    //fxn for display of placeholder
+    function blanks($word){
+      $place_holder = "";
+      for($i = 0; $i <= strlen($word); $i++){
+        $place_holder .= PLACEHOLDER;
+      }
+      return $place_holder;
+    }
+
+  }
--- index.php
+++ index.php
@@ -1,77 +1,27 @@
 <?php
+  $className = 'Hangman';
+spl_autoload_register(function ($class_name) {
+    include $class_name . '.php';
+});
+
   $word_list = file(PHPWS_SOURCE_DIR . 'mod/hangman/hangwords.txt');
   $word = $word_list[array_rand($word_list, 1)];
   $word = strtolower($word);
   $word = preg_replace('/[^a-z]/', '', $word);
-  $_SESSION['word_ses'] = $word;
-  $_SESSION['attempt'] = 0;
-  define('PLACEHOLDER', "_ ");
 
-  $template['FORM_CONTENT'] = 'Pick a letter: ';
-  $template['GREETING'] = 'Welcome to Hangman';
-  $template['IMG_SRC'] =
-    "http://localhost/phpwebsite/mod/hangman/img/hang{$_SESSION['attempt']}.gif";
-  $template['BLANKS_WORD'] = blanks('word_ses');
-  $template['ALPHABET'] = alphabet_list();
+  // $template['FORM_CONTENT'] = 'Pick a letter: ';
+  // $template['GREETING'] = 'Welcome to Hangman';
+  // $template['IMG_SRC'] =
+  //   "http://localhost/phpwebsite/mod/hangman/img/hang{$_SESSION['attempt']}.gif";
+  //$template['BLANKS_WORD'] = blanks('word_ses');
+  //$template['ALPHABET'] = alphabet_list();
 
   //$template['IMG_SRC'] = 'http://localhost/phpwebsite/mod/hangman/img/hang' . $attempts . 'gif';
 
-  echo PHPWS_Template::process($template, 'hangman','game.tpl');
+  //echo PHPWS_Template::process($template, 'hangman','game.tpl');
 
-  //fxn to run game
-  function run_game($word){
-    $word = $_SESSION['word_ses'];
-    $attempts = $_SESSION['attempt'];
-    $blank_spaces = blanks($word);
-
-      while($attempts > 0 && $attempts < 7){
-        $pos = strpos($word, $_GET['letter']);
-
-        if($pos === false){
-          $attempts = $attempts + 1;
-
-          $template['FORM CONTENT'] = 'Pick another letter: ';
-          $template['RESPONSE'] = 'Continue your game.
-            You have ' . (6 - $attempts) . ' attempts left.';
-          $template['IMG_SRC'] =
-            "http://localhost/phpwebsite/mod/hangman/img/hang$attempts.gif";
-          $template['BLANKS_WORD'] = $blank_spaces;
-          $template['GREETING'] = 'Your letter was not part of the word.';
-
-          echo PHPWS_Template::process($template, 'hangman','game.tpl');
-        }else{
-
-          for($i = 0; $i <= strlen($word); $i++){
-            if($word[$i] == $_GET['letter']){
-              $blank_spaces[$i] = $_GET['letter'];
-            }
-          }
-
-          $template['FORM CONTENT'] = 'Pick another letter: ';
-          $template['GREETING'] = 'Continue your game.
-            You have ' . (6 - $attempts) . ' attempts left.';
-          $template['IMG_SRC'] =
-            "http://localhost/phpwebsite/mod/hangman/img/hang$attempts.gif";
-          $template['BLANKS_WORD'] = $blank_spaces;
-          $template['RESPONSE'] = 'Your letter was found in the word!';
-
-          echo PHPWS_Template::process($template, 'hangman','game.tpl');
-        }
-      }
-
-      if($pos = strpos($blank_spaces, '_ ')){
-        $template['FORM CONTENT'] = 'You lost the game!';
-        $template['GREETING'] = 'Better luck next time.';
-        $template['IMG_SRC'] =
-          "http://localhost/phpwebsite/mod/hangman/img/hang6.gif";
-        $template['BLANKS_WORD'] = word_ses;
-        $template['RESPONSE'] = 'Click here to start a new game:
-          <a href="/phpwebsite/index.php?module=hangman">New Game</a>';
-
-        echo PHPWS_Template::process($template, 'hangman','game.tpl');
-      }
-    }
-
-run_game('word_ses');
+  // echo PHPWS_Template::process($template, 'hangman','game.tpl');
+  $gameNew = new Hangman($word, 0);
+  $_SESSION['game'] = $gameNew;
 
 ?>
